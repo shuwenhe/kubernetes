6fe46cf00a506adc0c22c42b0b21f0b07cb8277a
shuwen <1201220707@pku.edu.cn>
2025-07-05 10:13:13 +0800

    feat:worker install

---
+++ worker/chmod
@@ -0,0 +1,1 @@
+chmod +x ./install.sh
---
+++ worker/join.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.10.2:6443 --token 3zw2mz.lmbi48e9hs4psbsp --discovery-token-ca-cert-hash sha256:c1572e9718411eb61e9f208a5988e2a172712ab42ee640db7736b450d181a2a3
